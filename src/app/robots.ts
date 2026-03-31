06b57345a9d8749afe899b429d415ac3511618d7
MinSu <thoopring@gmail.com>
2026-03-31 16:13:54 +0900

    init

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://anchorlive.co.kr/sitemap.xml",
+  };
+}
